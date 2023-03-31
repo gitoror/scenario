95ddc533c2f88f92ee432fca70328790af89a1ec
tomrni <90772332+gitoror@users.noreply.github.com>
2023-03-31 14:22:49 +0200

    super

--- scenario.py
+++ scenario.py
@@ -2,53 +2,6 @@ import itertools as it
 import numpy as np
 
 
-def gen(L):
-    if len(L) == 1:
-        return L
-    else:
-        scenarios = []
-        for i in range(0, len(L)):
-            # Calc toutes les partitions de L\{e} en L1 ... Lk
-            for k in range(1, len(L)):
-                J = L.copy()
-                J.pop(i)
-                partitions = calc_partitions(J, k)
-                for partition in partitions:
-                    # Calc les ensembles d'arbres AL1 ... ALk pour la partition donnée
-                    ens_AL = []  # sera un ensemble d'ensembles de scenarios
-                    for j in range(0, k):
-                        ens_AL.append(gen(partition[j]))
-                    # Construire les scénarios
-                    add_scenarios(scenarios, ens_AL, V=L[i])
-        return scenarios
-
-
-def add_scenarios(scenarios, ens_AL, j=0, V=None):
-    print("j", j)
-    if j == 0:
-        for sous_scenario in ens_AL[j]:
-            scenarios.append([V, sous_scenario])
-        print("scenarios", scenarios)
-        print("")
-        add_scenarios(scenarios, ens_AL, j+1)
-    elif j < len(ens_AL):
-        new_scenarios = []
-        for scenario in scenarios:
-            print("scenario", scenario)
-            for sous_scenario in ens_AL[j]:
-                print("sous_scenario", sous_scenario)
-                sc = scenario.copy()
-                sc.append(sous_scenario)
-                new_scenarios.append(sc)
-                print(sc)
-        print("")
-        scenarios[:] = new_scenarios[:]  # garder même pointeur
-        add_scenarios(scenarios, ens_AL, j+1)
-    return
-
-# Idee garder toutes les combinaisons dont la reunion est J et l'intersection vide
-
-
 def calc_partitions(J):
     if len(J) == 1:
         return [[J]]
@@ -80,10 +33,175 @@ def calc_partitions(J):
         return partitions_ret
 
 
-P = calc_partitions([1, 2, 3, 4, 5])
-print("\nResult")
-for p in P:
-    print(p)
+def add_scenarios(scenarios, ens_AL, j=0, V=None):
+    if j == 0:
+        for sous_scenario in ens_AL[j]:
+            scenarios.append([V, sous_scenario])
+        add_scenarios(scenarios, ens_AL, j+1)
+    elif j < len(ens_AL):
+        new_scenarios = []
+        for scenario in scenarios:
+            for sous_scenario in ens_AL[j]:
+                sc = scenario.copy()
+                sc.append(sous_scenario)
+                new_scenarios.append(sc)
+        scenarios[:] = new_scenarios[:]  # garder même pointeur
+        add_scenarios(scenarios, ens_AL, j+1)
+    return
+
+
+def gen(L):
+    if len(L) == 1:
+        return L
+    else:
+        scenarios = []
+        for i in range(0, len(L)):
+            # Calc toutes les partitions de L\{e} en L1 ... Lk
+            for k in range(1, len(L)):
+                J = L.copy()
+                J.pop(i)
+                partitions = calc_partitions(J, k)
+                for partition in partitions:
+                    # Calc les ensembles d'arbres AL1 ... ALk pour la partition donnée
+                    ens_AL = []  # sera un ensemble d'ensembles de scenarios
+                    for j in range(0, k):
+                        ens_AL.append(gen(partition[j]))
+                    # Construire les scénarios
+                    add_scenarios(scenarios, ens_AL, V=L[i])
+        return scenarios
+
+
+def gen2(L):
+    print('-----------')
+    print("L", L)
+    if len(L) == 1:
+        print("single")
+        print("-")
+        return [L]
+    else:
+        scenarios = []
+        for i in range(0, len(L)):  # e=L[i]
+            # Calc toutes les partitions de L\{e} en L1 ... Lk
+            J = L.copy()
+            J.pop(i)
+            partitions = calc_partitions(J)
+            print("J", J)
+            # liste qui contiendra des ensembles de partitions
+            P = []
+            for u in range(len(L)-1):
+                P.append([])
+            for partition in partitions:
+                # P[0] ens de partitions à 1 partie
+                P[len(partition)-1].append(partition)
+            print("Calc P")
+            for p in range(len(P)):
+                print("k=", p+1, ":", P[p])
+            print("")
+            print("Calc scs")
+            for k in range(1, len(L)):  # k = nombre de parties
+                print("Pour k = ", k, "parties\n")
+                for partitions in P[k-1]:  # les partitions à k parties
+                    print("Pour ces partitions", partitions)
+                    # Calc les ensembles d'arbres AL1 ... ALk pour la partition donnée
+                    ens_AL = []  # sera un ensemble d'ensembles de scenarios
+                    for partition in partitions:
+                        print("Pour cette partition à k parties", partition)
+                        ens_AL.append(gen2(partition))
+                    # Construire les scénarios
+                    print("Ajout des scs")
+                    print("avant")
+                    for sc in scenarios:
+                        print(sc)
+                    add_scenarios(scenarios, ens_AL, V=L[i])
+                    print("Scs ajoutés !!!!!!! (apres)")
+                    for sc in scenarios:
+                        print(sc)
+                    print("--")
+        print("Fin de L=", L)
+        for sc in scenarios:
+            print(sc)
+        print("------------")
+
+        return scenarios
+
+
+# scs = gen2([1, 2, 3])
+# print("\nResult\n")
+# for sc in scs:
+#     print(sc)
+
+def gen3(L):
+    print('-----------')
+    print("L", L)
+    if len(L) == 1:
+        print("single")
+        print("-")
+        return [L]
+    else:
+        scenarios = []
+        for i in range(0, len(L)):  # e=L[i]
+            # Calc toutes les partitions de L\{e} en L1 ... Lk
+            J = L.copy()
+            J.pop(i)
+            partitions = calc_partitions(J)
+            print("J", J)
+            # liste qui contiendra des ensembles de partitions
+            P = []
+            for u in range(len(L)-1):
+                P.append([])
+            for partition in partitions:
+                # P[0] ens de partitions à 1 partie
+                P[len(partition)-1].append(partition)
+            print("Calc P")
+            for p in range(len(P)):
+                print("k=", p+1, ":", P[p])
+            print("")
+            print("Calc scs")
+            for k in range(1, len(L)):  # k = nombre de parties
+                print("Pour k = ", k, "parties\n")
+                for partitions in P[k-1]:  # les partitions à k parties
+                    print("Pour ces partitions", partitions)
+                    # Calc les ensembles d'arbres AL1 ... ALk pour la partition donnée
+                    ens_AL = []  # sera un ensemble d'ensembles de scenarios
+                    for partition in partitions:
+                        print("Pour cette partition à k parties", partition)
+                        ens_AL.append(gen3(partition))
+                    # Construire les scénarios
+                    print("Ajout des scs")
+                    print("avant")
+                    for sc in scenarios:
+                        print(sc)
+                    # add_scenarios(scenarios, ens_AL, V=L[i])
+                    combs = list(
+                        map(lambda x: list(x), list(it.product(*ens_AL))))
+                    for comb in combs:
+                        comb.insert(0, L[i])
+                        scenarios.append(comb)
+                    print("Scs ajoutés !!!!!!! (apres)")
+                    for sc in scenarios:
+                        print(sc)
+                    print("--")
+        print("Fin de L=", L)
+        for sc in scenarios:
+            print(sc)
+        print("------------")
+
+        return scenarios
+
+
+scs = gen3([1, 2, 3, 4, 5])
+print("\nResult\n")
+for sc in scs:
+    print(sc)
+print(len(scs))
+
+# Idee garder toutes les combinaisons dont la reunion est J et l'intersection vide
+
+
+# P = calc_partitions([1, 2, 3, 4, 5])
+# print("\nResult")
+# for p in P:
+#     print(p)
 
 
 def calc_risque(scenario, probas, gravites, pere=None):
